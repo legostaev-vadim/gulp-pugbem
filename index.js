cde2fe775eea37160217ec76b5dff76d13d24b56
Vadim <legostaev.vadim@mail.ru>
2018-04-04 23:19:51 +0300

    edit

--- index.js
+++ index.js
@@ -80,8 +80,18 @@ function pugbem(tokens) {
             else if (token.val.match(/^\-/)) {
 
                 if (!blockArray.length) return;
+                if (blockArray.length === 1 && element.line === blockArray[blockArray.length - 1].line) return;
 
                 modifier.line = token.loc.start.line;
+                if (modifier.line === tag.line) modifier.column = tag.column;
+                else modifier.column = token.loc.start.column;
+
+                for (let i = 0, length = blockArray.length; i < length; i++) {
+                    if (modifier.column <= blockArray[i].column) {
+                        blockArray.length = i;
+                        break;
+                    }
+                }
 
                 if (modifier.line === element.line) {
                     token.val = token.val.replace(/^\-\-?/, element.val + separator.mod);
